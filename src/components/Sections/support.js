f33e45738a08d155c2725e2cad8ce2959f1fa498
Theodora Milona <theodora.milona@cosee.biz>
2020-07-17 13:50:30 +0200

    Add illustration to support section (wip). Images still need to be optimized

--- src/components/Sections/support.js
+++ src/components/Sections/support.js
@@ -1,16 +1,24 @@
 import React from "react"
+import support_image from "../../assets/images/support.png"
 
 
 const SupportSection = () => {
     return (
         <div className="Support-container margin-left" id='support'>
-            <small className="subtitle white">Unterstützung</small>
-            <h2>Auch du kannst uns dabei helfen, CoraLibre zu verwirklichen!</h2>
-            <p>Die CoraLibre-App wird ehrenamtlich und durch Förderprojekte stetig weiterentwickelt.
-                Trotzdem brauchen wir eure Unterstützung und freuen uns über helfende Hände. Wenn du dich als Entwickler angesprochen fühlst, zögere nicht und nimm Kontakt zu uns auf.</p>
-            <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/CoraLibre/" className="button white-button">
-                Zum Github-Verzeichnis
-            </a></p>
+            <div className="Support-text-item">
+                <small className="subtitle white">Unterstützung</small>
+                <h2>Auch du kannst uns dabei helfen, CoraLibre zu verwirklichen!</h2>
+                <p>Die CoraLibre-App wird ehrenamtlich und durch Förderprojekte stetig weiterentwickelt.
+                    Trotzdem brauchen wir eure Unterstützung und freuen uns über helfende Hände. Wenn du dich als
+                    Entwickler angesprochen fühlst, zögere nicht und nimm Kontakt zu uns auf.</p>
+                <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/CoraLibre/"
+                      className="button white-button">
+                    Zum Github-Verzeichnis
+                </a></p>
+            </div>
+            <div className="Support-image-item">
+                <img src={support_image} alt="Eine Frau arbeitet am Computer" className="Support-image"/>
+            </div>
         </div>
     );
 };
