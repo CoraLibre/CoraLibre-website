e9e0360d7fe01b920fb4111b196316ab6363a652
Theodora Milona <theodora.milona@cosee.biz>
2020-07-17 13:33:24 +0200

    Replace icons with circular variants

--- src/components/Sections/reasons.js
+++ src/components/Sections/reasons.js
@@ -1,8 +1,8 @@
 import React from "react"
-import support_icon from "../../assets/images/support.png"
-import service_icon from "../../assets/images/services.png"
-import transparency_icon from "../../assets/images/transparency.png"
-import old_icon from "../../assets/images/old.png"
+import support_icon from "../../assets/images/support_icon.png"
+import service_icon from "../../assets/images/services_icon.png"
+import transparency_icon from "../../assets/images/transparency_icon.png"
+import old_icon from "../../assets/images/old_icon.png"
 
 const ReasonsSection = () => {
     return (
@@ -12,28 +12,28 @@ const ReasonsSection = () => {
             <div className="Reasons-grid">
                 <div className="Reasons-grid-item">
                     <img src={support_icon} alt="Support für andere Betriebssysteme"/>
-                    <h4>Support für andere Betriebssysteme</h4>
+                    <h3>Support für andere Betriebssysteme</h3>
                     <p>
                         CoraLibre ist auch für Nutzer zugänglich, die nicht das Android- oder Apple-Betriebssystem nutzen, sondern zum Beispiel LineageOS und /e/.
                     </p>
                 </div>
                 <div className="Reasons-grid-item">
                     <img src={service_icon} alt="CoraLibre arbeitet mit eigenen Services"/>
-                    <h4>Keine Google-Play-Services nötig</h4>
+                    <h3>Keine Google-Play-Services nötig</h3>
                     <p>
                     CoraLibre kommt ohne Google-Play-Services aus und bietet die selben Funktionen wie die offizielle
                     Corona-Warn-App.
                 </p></div>
                 <div className="Reasons-grid-item">
                     <img src={old_icon} alt="Älteres Modell eines Smartphones"/>
-                    <h4>Auch ältere Smartphones werden unterstützt</h4>
+                    <h3>Auch ältere Smartphones werden unterstützt</h3>
                     <p>
                         Auch Nutzer, die nicht über die benötigten System-Anforderungen für die offizielle App verfügen, können CoraLibre nutzen.
                     </p>
                 </div>
                 <div className="Reasons-grid-item">
                     <img src={transparency_icon} alt="Transparenter Quellcode"/>
-                    <h4>Transparenter Quellcode</h4>
+                    <h3>Transparenter Quellcode</h3>
                     <p>
                         Der Quellcode des offiziellen Google Tracing Protokolls ist nicht offen zugänglich, bei CoraLibre kann man diesen jedoch komplett einsehen.
                     </p>
