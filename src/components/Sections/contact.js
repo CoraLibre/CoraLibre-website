cc95dac00d3562fe2afe59e6a0c891bb402dd9de
Xerterhino <adribien94@googlemail.com>
2020-08-03 09:36:44 +0200

    Add media queries for displaysizes < 1000; Restyled reasons Grid to align text and icon next to each other; Moved contact image to left; Changed transparency_icon to new version

--- src/components/Sections/contact.js
+++ src/components/Sections/contact.js
@@ -7,14 +7,15 @@ const ContactSection = () => {
         <div className="Contact-container margin-left" id='contact'>
             <div className="Contact-grid">
                 <div>
+                    <img className="Contact-image" src={idea_image} alt="Illustration von einem Mann, dem Ideen durch den Kopf gehen"/>
+                </div>
+                <div>
                     <small className="subtitle">Kontakt</small>
                     <h2>Hast du noch Fragen, Wünsche, Anregungen?</h2>
                     <p>Wir suchen noch nach tatkräftiger Unterstützung in der App-Entwicklung.<br/>
                     Schreib uns gerne eine E-Mail an <a className="link" href="mailto:info@coralibre.de">info(at)coralibre.de</a>!</p>
                 </div>
-                <div>
-                    <img className="Contact-image" src={idea_image} alt="Illustration von einem Mann, dem Ideen durch den Kopf gehen"/>
-                </div>
+
             </div>
         </div>
     );
--- src/components/Sections/reasons.js
+++ src/components/Sections/reasons.js
@@ -12,31 +12,43 @@ const ReasonsSection = () => {
             <div className="Reasons-grid">
                 <div className="Reasons-grid-item">
                     <img src={support_icon} alt="Support für andere Betriebssysteme"/>
+                    <div className="Reasons-grid-item-text-wrapper">
                     <h3>Support für andere Betriebssysteme</h3>
                     <p>
                         CoraLibre ist auch für Nutzer zugänglich, die nicht das Android- oder Apple-Betriebssystem nutzen, sondern zum Beispiel LineageOS und /e/.
                     </p>
+                    </div>
                 </div>
                 <div className="Reasons-grid-item">
                     <img src={service_icon} alt="CoraLibre arbeitet mit eigenen Services"/>
+                    <div className="Reasons-grid-item-text-wrapper">
+
                     <h3>Keine Google-Play-Services nötig</h3>
                     <p>
                     CoraLibre kommt ohne Google-Play-Services aus und bietet die selben Funktionen wie die offizielle
                     Corona-Warn-App.
-                </p></div>
+                </p>
+                    </div>
+                </div>
                 <div className="Reasons-grid-item">
                     <img src={old_icon} alt="Älteres Modell eines Smartphones"/>
+                    <div className="Reasons-grid-item-text-wrapper">
+
                     <h3>Auch ältere Smartphones werden unterstützt</h3>
                     <p>
                         Auch Nutzer, die nicht über die benötigten System-Anforderungen für die offizielle App verfügen, können CoraLibre nutzen.
                     </p>
+                    </div>
                 </div>
                 <div className="Reasons-grid-item">
                     <img src={transparency_icon} alt="Transparenter Quellcode"/>
+                    <div className="Reasons-grid-item-text-wrapper">
+
                     <h3>Transparenter Quellcode</h3>
                     <p>
                         Der Quellcode des offiziellen Google Tracing Protokolls ist nicht offen zugänglich, bei CoraLibre kann man diesen jedoch komplett einsehen.
                     </p>
+                    </div>
                 </div>
             </div>
         </div>
