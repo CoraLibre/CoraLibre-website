0fee60695cdece54cfb437290a0007ac24e87103
Theodora Milona <theodora.milona@cosee.biz>
2020-08-14 11:57:38 +0200

    Add favicon and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,35 @@
 module.exports = {
-  plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
+    siteMetadata: {
+        siteUrl: `https://www.coralibre.de`,
     },
-  ],
+    plugins: [
+        'gatsby-plugin-resolve-src',
+        'gatsby-plugin-sass',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/assets/images/favicon.png",
+
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        }
+    ],
 }
