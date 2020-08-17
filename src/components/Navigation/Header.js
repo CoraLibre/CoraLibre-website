07611e0b3892e287b60bc6bb66f846a94273aa83
Theodora Milona <theodora.milona@cosee.biz>
2020-08-17 10:50:44 +0200

    Remove unnecessary section

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -6,53 +6,30 @@ import logo_small from "../../assets/images/logo_small-version.png"
 import Scrollspy from 'react-scrollspy'
 
 
-const Header = ({location}) => {
-    const pathname = location.href ? location.href : '';
-
-    if (pathname.includes(`datenschutz`)) {
-        return (
-            <div className="container">
-                    <Link to="/">
-                        <img src={logo_large} alt="CoraLibre Logo" className="logo-large"/>
-                        <img src={logo_small} alt="CoraLibre Logo" className="logo-small"/>
-                    </Link>
-
-                    <ul>
-                        <li>
-                            <Link to="/#reasons">Warum?</Link>
-                        </li>
-                        <li>
-                            <Link to="/#support">Unterstützung</Link>
-                        </li>
-                        <li>
-                            <Link to="/#contact">Kontakt</Link>
-                        </li>
-                    </ul>
-            </div>
-        );
-    } else
-        return (
-            <div className="container">
-                <Link to="/">
-                    <img src={logo_large} alt="CoraLibre Logo" className="logo-large"/>
-                    <img src={logo_small} alt="CoraLibre Logo" className="logo-small"/>
-                </Link>
-                <Scrollspy items={ ['quote', 'reasons', 'support', 'contact'] } currentClassName="is-current-section" scrolledPastClassName ="scrolled-past-section" offset={-250} className="scrollspy-container">
-                        <li>
-                            <AnchorLink href="#quote"/>
-                        </li>
-                        <li>
-                            <AnchorLink href="#reasons">Warum?</AnchorLink>
-                        </li>
-                        <li>
-                            <AnchorLink href="#support">Unterstützung</AnchorLink>
-                        </li>
-                        <li>
-                            <AnchorLink href="#contact">Kontakt</AnchorLink>
-                        </li>
-                    </Scrollspy>
-            </div>
-        );
+const Header = () => {
+    return (
+        <div className="container">
+            <Link to="/">
+                <img src={logo_large} alt="CoraLibre Logo" className="logo-large"/>
+                <img src={logo_small} alt="CoraLibre Logo" className="logo-small"/>
+            </Link>
+            <Scrollspy items={['quote', 'reasons', 'support', 'contact']} currentClassName="is-current-section"
+                       scrolledPastClassName="scrolled-past-section" offset={-250} className="scrollspy-container">
+                <li>
+                    <AnchorLink href="#quote"/>
+                </li>
+                <li>
+                    <AnchorLink href="#reasons">Warum?</AnchorLink>
+                </li>
+                <li>
+                    <AnchorLink href="#support">Unterstützung</AnchorLink>
+                </li>
+                <li>
+                    <AnchorLink href="#contact">Kontakt</AnchorLink>
+                </li>
+            </Scrollspy>
+        </div>
+    );
 }
 
 export default Header
