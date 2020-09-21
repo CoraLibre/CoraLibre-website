1faa931d7731ad514106672c6a8fc42f93dae8ba
Theodora Milona <theodora.milona@cosee.biz>
2020-09-21 14:29:31 +0200

    Rework navigation and fix link in footer for new page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
             </h3>
             <hr className="line white-border-top"/>
             <p><small>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.cosee.biz/impressum/">Impressum & Datenschutzerklärung</a><br/>
+                <Link to="/datenschutz">Impressum & Datenschutzerklärung</Link><br/>
                 &copy; {new Date().getFullYear()} CoraLibre<br/>
             </small>
             </p>
