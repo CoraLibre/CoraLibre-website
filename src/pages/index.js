daeee60a29be6feb7854c42869b3b0b9479f5058
Theodora Milona <theodora.milona@cosee.biz>
2020-11-06 15:27:47 +0100

    Add SEO translation

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,18 @@ import SupportSection from "../components/Sections/support"
 import HeroSection from "../components/Sections/hero"
 import QuoteSection from "../components/Sections/quote"
 import HessenSection from "../components/Sections/hessen"
+import {useTranslation} from 'gatsby-plugin-react-i18next'
 
 const IndexPage = () => {
+    const {t} = useTranslation();
+
     return (
         <Layout>
             <Helmet>
                 <meta charSet="utf-8" />
                 <html lang="de" />
                 <title>CoraLibre</title>
-                <meta name="description" content="Eine transparente OpenSource-Lösung und Alternative zur Corona-Warn-App ohne Google Services." />
+                <meta name="description" content={t('Eine transparente OpenSource-Lösung und Alternative zur Corona-Warn-App ohne Google Services.')} />
                 <meta name="theme-color" content="#1db5c4" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="keywords" content="CoraLibre, Corona-Warn-App, Google Play, Google Services, OpenSource, Alternative, OpenSource-Lösung, transparent" />
