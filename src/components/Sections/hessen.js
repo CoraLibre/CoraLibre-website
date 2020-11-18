04cca1fe4a16c8e2d30f4f9f11c4ed4ba0bf64cc
Theodora Milona <theodora.milona@cosee.biz>
2020-11-18 16:04:28 +0100

    Fix language key

--- src/components/Sections/hessen.js
+++ src/components/Sections/hessen.js
@@ -8,7 +8,7 @@ const HessenSection = () => {
                 <small className="subtitle white"><Trans>FÖRDERUNG</Trans></small>
                 <hr className="line white-border-top"/>
                 <h2><Trans>Dieses Projekt wird von der hessischen Landesregierung gefördert.</Trans></h2>
-                <p><Trans>Das Projekt CoraLibre wird finanziell durch das Land Hessen im Zeitraum vom 10.06.2020 bis 31.12.2020 unterstützt. Ziel des Vorhabens ist der Aufbau eines modernen Betriebs um die Verfügbarkeit der App zu gewährleisten sowie die internationale Vernetzung mit anderen Contract Tracing Apps voranzutreiben.</Trans></p>
+                <p><Trans>Das Projekt CoraLibre wird finanziell durch das Land Hessen im Zeitraum vom 10.06.2020 bis 31.12.2020 unterstützt. Ziel des Vorhabens ist der Aufbau eines modernen Betriebs, um die Verfügbarkeit der App zu gewährleisten, als auch die internationale Vernetzung mit anderen Contact-Tracing-Apps voranzutreiben.</Trans></p>
             </div>
 
         </div>
