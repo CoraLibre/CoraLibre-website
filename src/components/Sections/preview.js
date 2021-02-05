c4a9fa3ef28233c1d21b21ec04b71fdd0bf5adce
Michael Rascher <Michael.Rascher@cosee.biz>
2021-02-05 16:06:11 +0100

    added some text, that the add is still under development

--- src/components/Sections/preview.js
+++ src/components/Sections/preview.js
@@ -2,6 +2,7 @@ import React from "react"
 import AppImageOne from "../../assets/images/coralibre_Screenshot_01.png"
 import AppImageTwo from "../../assets/images/coralibre_Screenshot_02.png"
 import AppImageThree from "../../assets/images/coralibre_Screenshot_03.png"
+import {Trans, useTranslation} from 'gatsby-plugin-react-i18next'
 
 const HessenSection = () => {
     return (
@@ -12,6 +13,7 @@ const HessenSection = () => {
                 <img src={AppImageThree} alt={('Open-Source-Implementierung der Google-Tracing-API')}/>
                 <a target="_blank" rel="noopener noreferrer" download href="../../assests/download/CoraLibre.apk"
                    className="button green-button">Download CoraLibre.apk</a>
+                <span><Trans>Die CoraLibre App befindet sich noch in der Entwicklung, Abstürze und Fehler können an dieser Stelle noch auftreten.</Trans></span>
             </div>
 
         </div>
